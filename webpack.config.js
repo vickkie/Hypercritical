357e0911061c03b5bd2d0081c2b19a5e1374eefc
vickkie <vickkietrake@gmail.com>
2024-03-07 23:59:44 +0300

    verc

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   entry: "./js/main/main.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "public"),
   },
   module: {
     rules: [

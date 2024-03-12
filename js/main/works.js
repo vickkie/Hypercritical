27bf536622e1761dca43197a095cde6e7a502a48
vickkie <vickkietrake@gmail.com>
2024-03-12 03:53:13 +0300

    works mid-done

--- js/main/works.js
+++ js/main/works.js
@@ -1,7 +1,10 @@
 import MouseFollower from "mouse-follower";
 import Lenis from "@studio-freight/lenis";
 import { ScrollTrigger } from "gsap/all";
+
 import lozad from "lozad";
+import gsap from "gsap";
+import Reeller from "reeller";
 
 // App.js using react
 
@@ -86,3 +89,75 @@ document.addEventListener("DOMContentLoaded", function () {
   cursor();
   window.addEventListener("resize", cursor);
 });
+
+//Group 4: gsap scroll trigger
+
+gsap.registerPlugin(ScrollTrigger);
+
+let maintl = gsap.timeline();
+
+maintl.to("main", {
+  margin: 0,
+  ease: "none",
+  borderRadius: 0,
+});
+
+ScrollTrigger.create({
+  animation: maintl,
+  trigger: ".work-wrapper",
+  start: "bottom bottom",
+  end: "bottom center",
+  scrub: true,
+  markers: !true,
+  toggleActions: "play none none reverse",
+});
+
+//Group 5: mreeler-marquee
+
+Reeller.registerGSAP(gsap);
+
+const reeller = new Reeller({
+  container: ".sw-partner-marquee",
+  wrapper: ".sw-partner-marquee-row",
+  itemSelector: ".sw-partner-marquee-row-item",
+  speed: 40,
+});
+
+//Group 6: rolling text
+
+let elements = document.querySelectorAll(".rolling-text");
+
+elements.forEach((element) => {
+  let innerText = element.innerText;
+  element.innerHTML = "";
+
+  let textContainer = document.createElement("div");
+  textContainer.classList.add("block");
+
+  for (let letter of innerText) {
+    let span = document.createElement("span");
+    span.innerText = letter.trim() === "" ? "\xa0" : letter;
+    span.classList.add("letter");
+    textContainer.appendChild(span);
+  }
+
+  element.appendChild(textContainer);
+  element.appendChild(textContainer.cloneNode(true));
+
+  element.addEventListener("mouseover", () => {
+    // Add the 'hover' class to the hovered element
+    element.classList.add("hover");
+  });
+
+  element.addEventListener("mouseout", () => {
+    // Remove the 'hover' class when the mouse leaves the element
+    element.classList.remove("hover");
+  });
+});
+
+// For presentation purpose
+setTimeout(() => {
+  elements.forEach((element) => {
+    element.classList.add("play");
+  });
+}, 600);

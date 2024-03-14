0b2d5ec40fa7948568ed4baae6caf3be030cd178
vickkie <vickkietrake@gmail.com>
2024-03-14 16:49:46 +0300

    start studio

---
+++ js/main/studio.js
@@ -0,0 +1,196 @@
+import MouseFollower from "mouse-follower";
+import Lenis from "@studio-freight/lenis";
+import { ScrollTrigger } from "gsap/all";
+import lozad from "lozad";
+import Reeller from "reeller";
+// import gsap from "gsap";
+
+// App.js using react
+
+let select = (e) => document.querySelector(e);
+let selectAll = (e) => document.querySelectorAll(e);
+
+//group 0:: lozad lazyload ..
+
+document.addEventListener("DOMContentLoaded", function () {
+  const observer = lozad(".lozad", {
+    rootMargin: "200px 0px",
+    loaded: function (el) {
+      // console.log("Element loaded:", el);
+    },
+  });
+  observer.observe();
+});
+
+//   Group 1: smooth scroll
+
+let lenis = new Lenis({
+  duration: 3,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  direction: "vertical",
+  gestureDirection: "vertical",
+  smooth: true,
+  smoothTouch: false,
+  touchMultiplier: 2,
+  infinite: false,
+  autoResize: true,
+});
+
+lenis.on("scroll", (e) => {});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+//group 2: show and hide the scroll top button and scroll to top
+
+document.addEventListener("DOMContentLoaded", function () {
+  let toTopbutton = select("#toTop");
+
+  // Add an event listener to check scroll position
+  function showTotop() {
+    let scrollPosition = window.scrollY || document.documentElement.scrollTop;
+
+    if (scrollPosition > 1.2 * window.innerHeight) {
+      toTopbutton.style.display = "block";
+    } else {
+      toTopbutton.style.display = "none";
+    }
+  }
+  showTotop();
+  window.addEventListener("scroll", showTotop);
+
+  function scrollTop() {
+    gsap.to(window, {
+      duration: 2,
+      delay: 0,
+      scrollTo: {
+        y: 0,
+      },
+      ease: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    });
+  }
+
+  toTopbutton.addEventListener("click", scrollTop);
+});
+
+//Group 3: show mouse follower
+
+document.addEventListener("DOMContentLoaded", function () {
+  function cursor() {
+    if (innerWidth > 767) {
+      let cursorme = new MouseFollower();
+    }
+  }
+  cursor();
+  window.addEventListener("resize", cursor);
+});
+
+//Group 4: menuUzi midmoon
+let midmoon = select(".mid-moon");
+let menuclose = select(".action--close");
+let menuopen = select(".action--menuUzi");
+
+function open() {
+  midmoon.classList.add("mid-moon--light");
+  midmoon.classList.remove("mid-moon--dark");
+}
+// Close the menuUzi.
+function close() {
+  midmoon.classList.add("mid-moon--dark");
+  midmoon.classList.remove("mid-moon--light");
+}
+
+menuopen.addEventListener("click", open);
+menuclose.addEventListener("click", close);
+
+// Group 5: assign links for socials
+
+function assignLinks(config) {
+  for (const key in config) {
+    if (config.hasOwnProperty(key)) {
+      const elements = selectAll(key);
+      elements.forEach((element) => {
+        if (element) {
+          element.href = config[key];
+        } else {
+          console.error(`Elements matching selector ${key} not found.`);
+        }
+      });
+    }
+  }
+}
+
+new Promise((resolve, reject) => {
+  document.addEventListener("DOMContentLoaded", function () {
+    fetch("includes/config.json")
+      .then((response) => response.json())
+      .then((data) => {
+        assignLinks(data);
+        resolve();
+      })
+      .catch((error) => {
+        console.error("Error loading config:", error);
+        reject(error); // Reject the promise if there's an error`
+      });
+  });
+})
+  .then(() => {
+    // console.log("Configuration loaded and links assigned.");
+  })
+  .catch((error) => {
+    console.error("Failed to load configuration or assign links:", error);
+  });
+
+// Group 6: inverse the arrow colors using gsap
+
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const darkSections = gsap.utils.toArray(".white-section");
+  var arrowWrapper = select(".top-arrow-wrapper");
+  var arrowpath = select(".top-arrow-path");
+  var menuName = select(".menu-name");
+  var menuDotline = selectAll(".menu-dot-line");
+
+  function makedark() {
+    arrowpath.style.stroke = "var(--color-bg)";
+    arrowWrapper.style.fill = "var(--color-black)";
+    menuDotline.forEach((menuDotline) => {
+      menuDotline.style.background = "var(--color-black)";
+    });
+    menuName.style.color = "var(--color-black)";
+  }
+
+  function makewhite() {
+    arrowpath.style.stroke = "var(--color-black)";
+    arrowWrapper.style.fill = "var(--color-bg)";
+    menuDotline.forEach((menuDotline) => {
+      menuDotline.style.background = "var(--color-bg)";
+    });
+    menuName.style.color = "var(--color-bg)";
+  }
+  darkSections.forEach((darkSection, i) => {
+    //  let id = i;
+    //  console.log(id);
+
+    const darken = gsap.timeline({
+      scrollTrigger: {
+        trigger: darkSection,
+        id: i + 1,
+        start: "top top",
+        endtrigger: darkSection,
+        end: "bottom bottom",
+        scrub: true,
+        // markers: true,
+        onEnter: () => makewhite(),
+        onLeave: () => makedark(),
+        onEnterBack: () => makewhite(),
+        onLeaveBack: () => makedark(),
+      },
+    });
+  });
+});

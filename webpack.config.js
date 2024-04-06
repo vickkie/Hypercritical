386a34fb86a52324e9c17fadebd5a9711f1e49b6
vickkie <vickkie@users.noreply.github.com>
2024-04-06 15:05:20 +0000

    deploy: c900b074161b53dc90ed9461b15b1238628392ac

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
 const partytown = require("@builder.io/partytown/utils");
+const webpack = require("webpack");
 
 module.exports = {
   mode: "production",
@@ -187,6 +188,16 @@ module.exports = {
         removeRedundantAttributes: true,
         useShortDoctype: true,
       },
+    }),
+    new webpack.DefinePlugin({
+      "process.env.FIREBASE_API_KEY": JSON.stringify(process.env.FIREBASE_API_KEY),
+      "process.env.FIREBASE_AUTH_DOMAIN": JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+      "process.env.FIREBASE_PROJECT_ID": JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+      "process.env.FIREBASE_STORAGE_BUCKET": JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+      "process.env.FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+      "process.env.FIREBASE_APP_ID": JSON.stringify(process.env.FIREBASE_APP_ID),
+      "process.env.FIREBASE_MEASUREMENT_ID": JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID),
+      "process.env.FIREBASE_DATABASE_URL": JSON.stringify(process.env.FIREBASE_DATABASE_URL),
     }),
 
     new Dotenv(),

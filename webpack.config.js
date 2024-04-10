8bb72363e74c995b5e8f0337aa4be04aac7457b7
vickkie <vickkietrake@gmail.com>
2024-04-10 17:13:43 +0300

    dev/prod

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,26 @@ const Dotenv = require("dotenv-webpack");
 const partytown = require("@builder.io/partytown/utils");
 const webpack = require("webpack");
 
+// Check if the build is for production
+const isProduction = process.env.NODE_ENV === "production";
+
+const plugins = [];
+
+if (isProduction) {
+  plugins.push(
+    new webpack.DefinePlugin({
+      "process.env.FIREBASE_API_KEY": JSON.stringify(process.env.FIREBASE_API_KEY),
+      "process.env.FIREBASE_AUTH_DOMAIN": JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+      "process.env.FIREBASE_PROJECT_ID": JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+      "process.env.FIREBASE_STORAGE_BUCKET": JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+      "process.env.FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+      "process.env.FIREBASE_APP_ID": JSON.stringify(process.env.FIREBASE_APP_ID),
+      "process.env.FIREBASE_MEASUREMENT_ID": JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID),
+      "process.env.FIREBASE_DATABASE_URL": JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+    })
+  );
+}
+
 module.exports = {
   mode: "production",
   entry: {
@@ -189,18 +209,10 @@ module.exports = {
         useShortDoctype: true,
       },
     }),
-    new webpack.DefinePlugin({
-      "process.env.FIREBASE_API_KEY": JSON.stringify(process.env.FIREBASE_API_KEY),
-      "process.env.FIREBASE_AUTH_DOMAIN": JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-      "process.env.FIREBASE_PROJECT_ID": JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-      "process.env.FIREBASE_STORAGE_BUCKET": JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-      "process.env.FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
-      "process.env.FIREBASE_APP_ID": JSON.stringify(process.env.FIREBASE_APP_ID),
-      "process.env.FIREBASE_MEASUREMENT_ID": JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID),
-      "process.env.FIREBASE_DATABASE_URL": JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-    }),
 
-    new Dotenv(),
+    new Dotenv({
+      allowEmptyValues: true,
+    }),
     new HtmlMinimizerPlugin(),
     new CleanWebpackPlugin(),
   ],

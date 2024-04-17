8449f677f06e2c6676146dd2fcb3b27f942143de
vickkie <vickkietrake@gmail.com>
2024-04-17 03:15:02 +0300

    hash-route

--- js/main/components/LoginMain.jsx
+++ js/main/components/LoginMain.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './LoginPage';
 import Dashboard from './Dashboard'; // a Dashboard component
 import ErrorBoundary from './ErrorBoundary';

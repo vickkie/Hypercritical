62f14b74b119e9783d08820748d62a7ba538081a
vickkie <vickkietrake@gmail.com>
2024-04-17 15:38:04 +0300

    pre-split

--- js/main/components/LoginMain.jsx
+++ js/main/components/LoginMain.jsx
@@ -1,46 +1,47 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './LoginPage';
-import Dashboard from './Dashboard'; 
-import ErrorBoundary from './ErrorBoundary';
-import { AuthProvider } from './AuthContext';
-import PrivateRoute from './PrivateRoute';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./LoginPage";
+import Dashboard from "./Dashboard";
+import ErrorBoundary from "./ErrorBoundary";
+import { AuthProvider } from "./AuthContext";
+import PrivateRoute from "./PrivateRoute";
 
 const App = () => {
- return (
+  return (
     <Router>
-
       <ErrorBoundary>
-        <AuthProvider>    
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={
-            <PrivateRoute>
-              <Dashboard />
-           </PrivateRoute>
-          } />
-          {/* Redirect to login if not logged in */}
-          <Route path="*" element={<Navigate to="/login" />} />
-        </Routes>
+        <AuthProvider>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/dashboard"
+              element={
+                <PrivateRoute>
+                  <Dashboard />
+                </PrivateRoute>
+              }
+            />
+            {/* Redirect to login if not logged in */}
+            <Route path="*" element={<Navigate to="/login" />} />
+          </Routes>
         </AuthProvider>
       </ErrorBoundary>
     </Router>
- );
+  );
 };
 const renderApp = () => {
- const container = document.getElementById("loginpage");
- if (container) {
+  const container = document.getElementById("loginpage");
+  if (container) {
     const root = createRoot(container);
-    root.render(   
-             
-      <React.StrictMode> 
-         <App />          
+    root.render(
+      <React.StrictMode>
+        <App />
       </React.StrictMode>
     );
- } else {
+  } else {
     console.error("Element with your root id is not found");
- }
+  }
 };
 
 document.addEventListener("DOMContentLoaded", renderApp);
--- public/js/main/components/Dashboard.jsx
+++ public/js/main/components/Dashboard.jsx
@@ -1,70 +1,117 @@
-import React, { useEffect , useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useAuth } from './AuthContext'; // Ensure this path is correct
-import { getAuth, signOut } from 'firebase/auth'; // Import signOut from Firebase
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "./AuthContext";
+import { getAuth, signOut } from "firebase/auth"; // Import signOut from Firebase
+import { getDatabase, ref, onValue, off } from "firebase/database";
+import Container from "@mui/material/Container";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import TablePagination from "@mui/material/TablePagination";
+import Table from "@mui/material/Table";
+
+import ResponsiveAppBar from "./elements/Header";
 
 const Dashboard = () => {
- const { currentUser } = useAuth();
- const auth = getAuth();
- const navigate = useNavigate();
+  const { currentUser } = useAuth();
+  const auth = getAuth();
+  const navigate = useNavigate();
 
-  const [messo, setMesso] = useState('');
+  const [consultations, setConsultations] = useState({});
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
 
- useEffect(() => {
-    const fetchData = async () => {
-      try {     
-        const result = await axios.get("https://hypercritical-api.vercel.app");
-        setMesso(result.data.messo);
-      } catch (error) {
-        console.error('Error fetching data:', error);
+  useEffect(() => {
+    const db = getDatabase();
+    const consultationRef = ref(db, "consultations");
+
+    const handleData = (snapshot) => {
+      if (snapshot.exists()) {
+        const data = snapshot.val();
+        setConsultations(data);
+      } else {
+        console.log("No data available");
+        setConsultations({});
       }
-   
     };
-    fetchData();
- }, []);
 
- // Prevent reload functionality
- useEffect(() => {
-    if (currentUser) {
-      const handleBeforeUnload = (event) => {
-        event.preventDefault();
-        event.returnValue = '';
-      };
+    onValue(consultationRef, handleData, (error) => {
+      console.error("Error fetching data:", error);
+    });
 
-      window.addEventListener('beforeunload', handleBeforeUnload);
+    return () => {
+      off(consultationRef, "value", handleData);
+    };
+  }, []);
 
-      return () => {
-        window.removeEventListener('beforeunload', handleBeforeUnload);
-      };
-    }
- }, [currentUser]); // Depend on currentUser to re-run the effect when it changes
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
 
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
-
- 
- if (!currentUser) {
-    navigate('/login');
+  if (!currentUser) {
+    navigate("/login");
     return null;
- }
+  }
 
- const handleLogout = async () => {
+  const handleLogout = async () => {
     try {
       await signOut(auth);
-      navigate('/login'); // Redirect to login page after logout
+      navigate("/login"); // Redirect to login page after logout
     } catch (error) {
       console.error("Logout failed:", error);
     }
- };
+  };
 
- return (
+  const consultationRows = Object.values(consultations).map((consultation, index) => (
+    <TableRow key={index}>
+      <TableCell>{consultation.name}</TableCell>
+      <TableCell>{consultation.email}</TableCell>
+      <TableCell>{consultation.budget}</TableCell>
+      <TableCell>{consultation.consultationType}</TableCell>
+      <TableCell>{new Date(consultation.date).toLocaleDateString()}</TableCell>
+      <TableCell>{consultation.message}</TableCell>
+    </TableRow>
+  ));
+
+  return (
     <div>
-      <h1>Welcome to the Dashboard</h1>
-      <p>You are now logged in.</p>
-      <h1>{messo}</h1>
-      <button onClick={handleLogout}>Logout</button>
+      <ResponsiveAppBar onLogout={handleLogout} />
+      <Container maxWidth="lg">
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>Email</TableCell>
+                <TableCell>Budget</TableCell>
+                <TableCell>Consultation Type</TableCell>
+                <TableCell>Date</TableCell>
+                <TableCell>Message</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>{consultationRows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)}</TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={consultationRows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Container>
     </div>
- );
+  );
 };
 
 export default Dashboard;

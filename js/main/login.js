bdbc0ec028551f81ddf5b8f496749fd61afd66dd
vickkie <vickkietrake@gmail.com>
2024-06-15 18:09:10 +0300

    dashboard menu

--- js/main/login.js
+++ js/main/login.js
@@ -5,6 +5,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const titlePage = select(".nav_logo_parent");
   const email = select(".inputLogin1");
   const password = select(".inputLogin2");
+  const passwordInput = select(".inputLogin2");
+  console.log(passwordInput);
 
   if (matchMedia("screen and (max-width: 768px)").matches) {
     titlePage.innerHTML = "H.C";
